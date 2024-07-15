9ce8085b66da6265630a499385f65e44630abcb5
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2024-07-15 15:36:15 +0530

    add gridview builder widget to show category

--- lib/pages/categories/grid_category.dart
+++ lib/pages/categories/grid_category.dart
@@ -8,6 +8,28 @@ class GridCategory extends StatefulWidget {
 }
 
 class _GridCategoryState extends State<GridCategory> {
+
+  //temporary List of category
+  final List<Map<String, dynamic>> categories = [
+    {
+      'image': "assets/images/category_page/new_arrival.png",
+    },
+    {
+      'image': "assets/images/category_page/cloth.png",
+    },
+    {
+      'image': "assets/images/category_page/bags.png",
+    },
+    {
+      'image': "assets/images/category_page/shoes.png",
+    },
+    {
+      'image': "assets/images/category_page/electronics.png",
+    },
+    {
+      'image': "assets/images/category_page/jewelry.png",
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,28 +57,28 @@ class _GridCategoryState extends State<GridCategory> {
             ),
 
             //grid view for category
-            GridView.count(
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // number of items in each row
+                mainAxisSpacing: 15.0, // spacing between rows
+                crossAxisSpacing: 15.0, // spacing between columns
+              ),
               shrinkWrap: true,
-              crossAxisCount: 2,
-              padding: EdgeInsets.only(top: 20.0),
-              scrollDirection: Axis.vertical,
-              mainAxisSpacing: 15.0,
-              crossAxisSpacing: 15.0,
-              children: List.generate(6, (index) {
+              padding: EdgeInsets.all(8.0), // padding around the grid
+              itemCount: categories.length, // total number of items
+              itemBuilder: (context, index) {
+                final category = categories[index];
                 return Container(
                   width: 200,
                   height: 200,
                   decoration: BoxDecoration(
                       image: DecorationImage(
-                          image: AssetImage(
-                            "assets/images/category_page/bags.png",
-                          ),
+                          image: AssetImage(category['image']),
                           fit: BoxFit.cover),
                       color: Colors.grey,
                       borderRadius: BorderRadius.circular(12)),
-                  child: Container()
                 );
-              }),
+              },
             )
           ],
         ),
